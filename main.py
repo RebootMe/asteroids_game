60a008d35109ed401bea9c59f8aec8122b259f2c
Quad98 <zeeshan98@gmail.com>
2024-10-09 20:01:53 +0500

    Black screen opening

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
 from constants import *
-
+BLACK = (0,0,0)
 
 def main():
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		screen.fill(BLACK, rect=None, special_flags=0)
+		pygame.display.flip()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 if __name__ == "__main__":
 		main()
